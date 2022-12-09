1f9da389a5b1476ae8d67d9e1d4cc1a49ec1a03a
artembaranov <artbaranov@storytelling-software.com>
2022-12-09 12:39:19 +0300

    Scan ble devices

--- app/src/main/java/storytelling/apps/testbluetooth/MainActivity.kt
+++ app/src/main/java/storytelling/apps/testbluetooth/MainActivity.kt
@@ -5,12 +5,16 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_FINISHED
 import android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_STARTED
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -35,6 +39,11 @@ import storytelling.apps.testbluetooth.ui.theme.TestBluetoothTheme
 class MainActivity : ComponentActivity() {
 
     private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private val SCAN_PERIOD: Long = 20000
+    private var scanning = false
+    private val handler = Handler()
+
 
     data class UiState(
         var list: List<String?> = emptyList()
@@ -45,31 +54,31 @@ class MainActivity : ComponentActivity() {
 
     private val list1 = mutableListOf<String?>()
 
-
-
-    private val receiver = object : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            when (intent.action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    checkAndRequestPermission()
-                    val deviceName = device?.address
-                    list1.add(deviceName.toString())
-                    uiState = uiState.copy(list = list1)
-                }
-                ACTION_DISCOVERY_STARTED -> {
-                    Log.d("Device", "Discovery Started")
-                }
-                ACTION_DISCOVERY_FINISHED -> {
-                    Log.d("Device", "Discovery Finished")
-                    for (i in uiState.list) {
-                        Log.d("Device", i.toString())
-                    }
-                }
-            }
-        }
-    }
+//
+//    private val receiver = object : BroadcastReceiver() {
+//
+//        override fun onReceive(context: Context, intent: Intent) {
+//            when (intent.action) {
+//                BluetoothDevice.ACTION_FOUND -> {
+//                    val device: BluetoothDevice? =
+//                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+//                    checkAndRequestPermission()
+//                    val deviceName = device?.address
+//                    list1.add(deviceName.toString())
+//                    uiState = uiState.copy(list = list1)
+//                }
+//                ACTION_DISCOVERY_STARTED -> {
+//                    Log.d("Device", "Discovery Started")
+//                }
+//                ACTION_DISCOVERY_FINISHED -> {
+//                    Log.d("Device", "Discovery Finished")
+//                    for (i in uiState.list) {
+//                        Log.d("Device", i.toString())
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,13 +86,13 @@ class MainActivity : ComponentActivity() {
         initManagers()
         checkAndRequestPermission()
         checkBluetoothIsEnabled()
-        val filter1 = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        val filter2 = IntentFilter(ACTION_DISCOVERY_STARTED)
-        val filter3 = IntentFilter(ACTION_DISCOVERY_FINISHED)
+//        val filter1 = IntentFilter(BluetoothDevice.ACTION_FOUND)
+//        val filter2 = IntentFilter(ACTION_DISCOVERY_STARTED)
+//        val filter3 = IntentFilter(ACTION_DISCOVERY_FINISHED)
 
-        registerReceiver(receiver, filter1)
-        registerReceiver(receiver, filter2)
-        registerReceiver(receiver, filter3)
+//        registerReceiver(receiver, filter1)
+//        registerReceiver(receiver, filter2)
+//        registerReceiver(receiver, filter3)
 
         setContent {
             TestBluetoothTheme {
@@ -120,6 +129,45 @@ class MainActivity : ComponentActivity() {
 
     private fun initManagers() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            checkAndRequestPermission()
+            val deviceName = result.device.name
+            if (deviceName == null){
+                list1.add("null")
+            }
+            else{
+                list1.add(result.device.name)
+            }
+        }
+    }
+
+    private fun scanLeDevice() {
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                checkAndRequestPermission()
+                bluetoothLeScanner.stopScan(leScanCallback)
+                Log.d("Device","Scan cancelled 1")
+                for (i in list1){
+                    Log.d("Device", i.toString())
+                }
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        } else {
+            Log.d("Device","Scan cancelled 2")
+            for (i in list1){
+                Log.d("Device", i.toString())
+            }
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+
+        }
     }
 
     @Composable
@@ -144,18 +192,18 @@ class MainActivity : ComponentActivity() {
             Button(
                 onClick = {
                     checkAndRequestPermission()
-                    val discoveryAvailable = bluetoothAdapter.startDiscovery()
-                    Log.d("Device", "Is discovery available: $discoveryAvailable")
+                    scanLeDevice()
+                    Log.d("Device", "Scanning started")
                 }) {
                 Text(text = "StartSearch")
             }
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        unregisterReceiver(receiver)
-    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//
+//        unregisterReceiver(receiver)
+//    }
 }
 
